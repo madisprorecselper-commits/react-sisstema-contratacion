c23997e6bcec5937f1f19ea48ba2f3b518b9f97c
Kerith <kyinethrodriguez@gmail.com>
2025-10-02 12:55:40 -0500

    corregido

--- frontend/src/components/Welcomemessage.jsx
+++ frontend/src/components/Welcomemessage.jsx
@@ -12,11 +12,11 @@ export function WelcomeMessage({ name }) {
       <h3
         className="text-4xl font-extrabold bg-gradient-to-r from-yellow-800 via-orange-700 to-amber-400 bg-clip-text text-transparent drop-shadow-lg"
       >
-        ¡Bienvenido, {name}! 👋
+        ¡Bienvenido, {name}! 
       </h3>
 
       <p className="text-gray-700 font-medium mt-2">
-        Nos alegra verte de nuevo 🚀
+        Nos alegra verte de nuevo 
       </p>
     </motion.div>
   );
