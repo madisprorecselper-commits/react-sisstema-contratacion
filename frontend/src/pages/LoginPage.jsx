6034d7fbf9a9d8b511ac29f2fd7b0465310633a3
Angel Oyaga <santioyaga10@gmail.com>
2025-10-24 19:23:50 -0500

    Primeros Pasos

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { useNavigate, Link } from "react-router-dom"
-import { NavBar } from "../components/NavBar"
+import logoUno from "../assets/Logo.jpeg"
+import logoDos from "../assets/MadeproDismapro.png"
 import { WelcomeMessage } from "../components/WelcomeMessage"
 
 export function LoginPage() {
@@ -10,17 +11,6 @@ export function LoginPage() {
   const [error, setError] = useState("")
   const [isLogged, setIsLogged] = useState(false)
   const navigate = useNavigate()
-
-  // Estilos dinámicos para los botones de rol
-  const buttonUser =
-    role === "usuario"
-      ? "bg-yellow-500 text-white px-4 py-2 rounded"
-      : "bg-orange-900 text-white px-4 py-2 rounded"
-
-  const buttonAdministrador =
-    role === "administrador"
-      ? "bg-yellow-500 text-white px-4 py-2 rounded"
-      : "bg-orange-900 text-white px-4 py-2 rounded"
 
   const handleUserClick = () => setRole("usuario")
   const handleAdminClick = () => setRole("administrador")
@@ -71,43 +61,69 @@ export function LoginPage() {
 
   return (
     <>
-      <NavBar />
 
       <div className="container mx-auto px-4 mt-20">
-        <div className="max-w-md mx-auto bg-gray-200 p-6 rounded-lg shadow-md">
-          <h2 className="text-center text-xl font-semibold text-gray-800 mb-6">
+        <div className="max-w-md mx-auto bg-white p-6 rounded-lg shadow-md">
+        {/*<div className="flex justify-center" >
+            <img src={logoDos} alt="Logo 2" className="mx-auto max-w-sm h-60" /> 
+          </div>*/} 
+           
+          <h1 className="text-center text-3xl font-black text-gray-800 mb-4">Welcome Back</h1>
+          <h2 className="text-center text-xl font-light text-gray-500 mb-4">
             Iniciar Sesión
           </h2>
-
+          
+ 
           {/* Botones de selección de rol */}
-          <div className="flex justify-center flex-wrap gap-3 mb-6">
-            <button className={buttonUser} onClick={handleUserClick}>
-              Usuario
-            </button>
-            <button className={buttonAdministrador} onClick={handleAdminClick}>
-              Administrador
-            </button>
-          </div>
+          <div className="flex justify-center mb-6">
+  <div className="flex bg-gray-100 rounded-lg shadow-sm overflow-hidden">
+    <button
+      onClick={handleUserClick}
+      className={`px-6 py-2 text-sm font-semibold transition-colors duration-200 ${
+        role === "usuario"
+          ? "bg-white text-yellow-400 shadow-inner"
+          : "text-gray-500 hover:text-gray-700"
+      }`}
+    >
+      Usuario
+    </button>
+
+    <button
+      onClick={handleAdminClick}
+      className={`px-6 py-2 text-sm font-semibold transition-colors duration-200 ${
+        role === "administrador"
+          ? "bg-white text-yellow-400 shadow-inner"
+          : "text-gray-500 hover:text-gray-700"
+      }`}
+    >
+      Administrador
+    </button>
+  </div>
+</div>
+
+
 
           {/* Formulario */}
           <form onSubmit={handleSubmit}>
             <div className="mb-4">
+              <h2 className="mb-4 text-gray-800 font-black">Email Adress</h2>
               <input
                 type="text"
                 placeholder="Usuario"
                 value={UserName}
                 onChange={(e) => setUserName(e.target.value)}
-                className="w-full px-4 py-2 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-orange-700"
+                className="w-full px-4 py-2 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-100"
               />
             </div>
 
             <div className="mb-4">
+                <h2 className="mb-4 text-gray-800 font-bold">PassWord</h2>
               <input
                 type="password"
                 placeholder="Contraseña"
                 value={PassWord}
                 onChange={(e) => setPassWord(e.target.value)}
-                className="w-full px-4 py-2 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-orange-700"
+                className="w-full px-4 py-2 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-100"
               />
             </div>
 
@@ -115,27 +131,30 @@ export function LoginPage() {
             {error && <p className="text-red-600 text-sm mb-4">{error}</p>}
 
             <div className="text-center mb-4">
-              <Link
-                to="/recuperar"
-                className="text-orange-900 hover:underline text-sm"
+              <button
+                type="submit"
+                className="bg-gray-800 w-sm text-white px-4 py-2 rounded-lg text-sm hover:bg-yellow-200 hover:text-gray-800 transition-colors duration-300"
               >
-                ¿Olvidó su contraseña?
-              </Link>
+                Iniciar Sesión
+              </button>
+           
             </div>
 
             <div className="flex justify-center gap-4">
               <Link
                 to="/registro"
-                className="bg-orange-900 text-white px-4 py-2 rounded text-sm hover:bg-orange-800"
+                className="text-gray-500 hover:underline text-sm0"
               >
                 Registrarse
               </Link>
-              <button
-                type="submit"
-                className="bg-orange-900 text-white px-4 py-2 rounded text-sm hover:bg-orange-800"
+
+                 <Link
+                to="/recuperar"
+                className="text-gray-800 hover:underline text-sm"
               >
-                Iniciar Sesión
-              </button>
+                ¿Olvidó su contraseña?
+              </Link>
+              
             </div>
           </form>
         </div>
