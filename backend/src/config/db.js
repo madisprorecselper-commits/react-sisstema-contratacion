88c01fba42fe48ac99298f60935fccf34cfd5719
Angel Oyaga <santioyaga10@gmail.com>
2025-11-13 12:55:27 -0500

    Despliegue en render

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,19 +1,21 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
-  host: "switchyard.proxy.rlwy.net",
-  user: "root",
-  password: "onqsiGvVuSHxAVnNZzsEzxCPVmhoethu",
-  database: "railway",
-  port: 33293,
+const connection = mysql.createPool({
+  host: process.env.DB_HOST || "bezi4vq23ekf9a7bzhlp-mysql.services.clever-cloud.com",
+  user: process.env.DB_USER || "u6mzm7jalmktaj1s",
+  password: process.env.DB_PASS || "LHLaQkMItsfY20M766p1",
+  database: process.env.DB_NAME || "bezi4vq23ekf9a7bzhlp",
+  port: process.env.DB_PORT || 3306,
+  connectionLimit: 10,
 });
 
-connection.connect((err) => {
+connection.getConnection((err, conn) => {
   if (err) {
-    console.error("Error conectando a la base de datos/:", err);
+    console.error("Error de conexión", err);
     return;
   }
-  console.log("Conectado exitosamente a la base de datos Railway/");
+  console.log("Conectado exitosa");
+  conn.release();
 });
 
 module.exports = connection;
