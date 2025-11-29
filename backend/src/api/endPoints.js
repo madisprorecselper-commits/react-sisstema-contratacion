b96c14f838da1a7ba0d96ed8484825bda79f4a24
Angel Oyaga <santioyaga10@gmail.com>
2025-11-29 10:49:04 -0500

    Feature/ Boton para aplicar

--- backend/src/api/endPoints.js
+++ backend/src/api/endPoints.js
@@ -4,11 +4,13 @@ const router = express.Router();
 // controladores
 const { login, registro } = require('../controllers/usuarioController');
 const { vacantes, obtenerVacantes } = require('../controllers/vacanteController');
+const { postulacion } = require('../controllers/postulacionController')
 
 // RUTAS
 router.post('/login', login);
 router.post('/registro', registro);
 router.post('/vacantes', vacantes);
+router.post('/postulacion', postulacion);
 router.get('/vacantes', obtenerVacantes);
 
 module.exports = router;
